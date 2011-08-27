524f5caa21904acc21c9b504482dadc4002df53b
Thomas Quémard <thomas.quemard@gmail.com>
2011-08-27 21:20:05 +0200

    Used Apple stock price in line_area example (more dense data)

--- examples/line_area.c
+++ examples/line_area.c
@@ -5,17 +5,24 @@
 int main (int argc, char** argv)
 {
    sparks_data_t data[] = {
-      10, 100, 30, 50, 70, 117, 80, 120, 10, 20,
-      50, 40, 20, 100, 110, 80, 120, 90, 100, 110
+      17, 12, 11, 12, 6, 0, 2, 14, 11, 9,
+      12, 8, 9, 5, 9, 14, 17, 20, 31, 36,
+      31, 36, 34, 39, 39, 34, 34, 36, 33, 29,
+      34, 31, 26, 40, 36, 29, 14, 22, 26, 29,
+      16, 5, 9, 11, 16, 5, 11, 8, 11, 9,
+      16, 17, 22, 22, 22, 20, 17, 12, 19, 20,
+      25, 31, 31, 31, 34, 47, 47, 53, 56, 57,
+      59, 54, 56, 54, 56, 51, 56, 60, 62, 59,
+      65, 67, 73, 71, 73, 78, 79, 81, 79, 82,
+      82, 82, 88, 93, 90, 90, 87, 109, 121, 127
    };
-
    sparks_area_opt_t opt = sparks_area_opt_new();
    opt.color = sparks_get_rgb_color(0.9, 0.9, 0.9);
 
    sparks_t graph = sparks_new(150, 20);
-   sparks_set_data_length(&graph, 20);
-   sparks_draw_area(&graph, data, 20, opt);
-   sparks_draw_simple_line(&graph, data, 20);
+   sparks_set_data_length(&graph, 100);
+   sparks_draw_area(&graph, data, 100, opt);
+   sparks_draw_simple_line(&graph, data, 100);
    sparks_save_png(&graph, "line_area.png");
    sparks_clean(&graph);
 
